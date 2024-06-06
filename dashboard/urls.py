d157689f08a436ca996d902e7f6f9377d2010d85
Luna <lunammarquez@gmail.com>
2024-06-06 18:12:13 -0300

    added admin_panel funcionalities. 'staff' is now 'admin'

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,6 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+     # index login page
      path('', views.index, name='index'),
-     path('staff/', views.staff, name='staff')
+     # admin panel
+     path('admin_panel/', views.admin_panel, name='admin_panel'),
+     path('admin_panel/payment', views.admin_panel_payment, name='admin_panel_payment'),
+     path('admin_panel/users', views.admin_panel_users, name='admin_panel_users'),
+     path('admin_panel/products', views.admin_panel_products, name='admin_panel_products'),
+     path('admin_panel/delete_sale', views.admin_panel_delete_sale, name='admin_panel_delete_sale'),
+     # stock
+     path('stock/', views.stock, name='stock'),
+     path('sales/', views.sales, name='sales'),
+     path('reports/', views.reports, name='reports'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -3,10 +3,33 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+# index login page
 def index(request):
-    return render(request, 'dashboard/index.html')
-    
+    return render(request, 'index.html')
+
+# admin panel
+def admin_panel(request):
+    return render(request, 'admin_panel.html')
+
+def admin_panel_payment(request):
+    return render(request, 'admin_panel/payment.html')
+
+def admin_panel_users(request):
+    return render(request, 'admin_panel/users.html')
+
+def admin_panel_products(request):
+    return render(request, 'admin_panel/products.html')
+
+def admin_panel_delete_sale(request):
+    return render(request, 'admin_panel/delete_sale.html')
 
 
-def staff(request):
-    return render(request, 'dashboard/staff.html')
+# stock
+def stock(request):
+    return render(request, 'stock.html')
+
+def sales(request):
+    return render(request, 'sales.html')
+
+def reports(request):
+    return render(request, 'reports.html')
